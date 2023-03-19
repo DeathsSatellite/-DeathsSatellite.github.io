fc43fd1981029c9a15105e9b7aa066ceb3f8c599
DeathsSatellite <82217560+DeathsSatellite@users.noreply.github.com>
2023-03-19 14:33:36 -0400

    Update
    Replaced files

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-var count = 10; // The delay in seconds before redirecting.
+var count = 7; // The delay in seconds before redirecting.
 var redirect = "https://www.carnivalhunt.com/"; // The redirection's landing page.
 
 function countDown() {
-  if(count >= 0){
-    document.getElementById("timer").innerHTML = count--;
-    setTimeout("countDown()", 1000);
-  }else{
-    window.location.href = redirect;
-  }
+	if(count >= 0){
+		document.getElementById("timer").innerHTML = count--;
+		setTimeout(countDown, 1000);
+	}else{
+		window.location.href = redirect;
+	}
 }
+
 countDown();
